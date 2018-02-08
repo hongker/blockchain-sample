0c08b04349ea1388f814a7502b55cd711c6d6403
hongker <xiaok2013@live.com>
2018-02-08 17:29:31 +0800

    添加流拍卖程

--- client/src/views/Auction.vue
+++ client/src/views/Auction.vue
@@ -24,10 +24,21 @@
       <div style="padding: 14px;">
         <span>{{item.title}}</span>
         <span class="price"><strong class="red">{{item.price}}</strong></span>
+       
         <div class="bottom clearfix">
-          <time class="time">{{ item.date }}</time>
-          <el-button type="danger" class="button" size="medium" @click="auction(index)">出价</el-button>
+        	
+        	<el-button v-if="user.id==item.userId && item.state==0" type="danger" class="button" size="mini" @click="up(index)">上架</el-button>
+
+        	<el-button v-if="user.id==item.userId && item.state==1" type="danger" class="button" size="mini" @click="down(index)">下架</el-button>
+          <el-button v-if="user.id==item.userId && item.state==1" type="info" class="button" size="mini" @click="start(index)">开始</el-button>
+
+          	<el-button v-if="user.id==item.userId && item.state==2" type="danger" class="button" size="mini" @click="end(index)">结束</el-button>
+          <el-button v-if="item.state==2" type="danger" class="button" size="mini" @click="auction(index)">出价</el-button>
+
+          	<p v-if="item.state==3">已结束</p>
         </div>
+        
+        
       </div>
     </el-card>
   </el-col>
@@ -42,6 +53,9 @@
 			return {
 				activeIndex2: '1',
 				currentDate: '02-08 14:50:00',
+				user: {
+					id:10001,
+				},
 				goods: [
 					{
 						id: 1,
@@ -49,6 +63,8 @@
 						title: "好吃的汉堡",
 						date:"02-08 14:50:00",
 						price: 200.00,
+						userId: 10001,
+						state: 0,
 					},
 					{
 						id: 2,
@@ -56,11 +72,43 @@
 						title: "好吃的汉堡",
 						date:"02-08 14:50:00",
 						price: 200.00,
+						userId: 10002,
+						state: 0,
 					}
 				],
 			}
 		},
 		methods: {
+			up(index) {
+				this.$message({
+		            type: 'success',
+		            message: '上架成功'
+		          }); 
+				this.goods[index].state = 1;
+        		
+			},
+			start(index) {
+				this.$message({
+		            type: 'success',
+		            message: '启动成功'
+		          }); 
+				this.goods[index].state = 2;
+        		
+			},
+			down(index) {
+				this.$message({
+		            type: 'success',
+		            message: '下架成功'
+		          }); 
+				this.goods[index].state = 0;
+			},
+			end(index) {
+				this.$message({
+		            type: 'success',
+		            message: '结束成功'
+		          }); 
+				this.goods[index].state = 3;
+			},
       auction(index) {
         this.$prompt('请输入你的价格', '提示', {
           confirmButtonText: '确定',
@@ -104,6 +152,7 @@
 
   .button {
     float: right;
+    margin-left: 5px;
   }
 
   .image {
